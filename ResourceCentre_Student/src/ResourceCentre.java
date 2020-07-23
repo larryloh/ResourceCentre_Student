c3daccbc06ba2f0b10214a54fc1cc6220a68dea0
eng70 <eng70@LAPTOP-8KF6JNF5>
2020-07-23 11:19:49 +0800

    I change comment at line 33

--- ResourceCentre_Student/src/ResourceCentre.java
+++ ResourceCentre_Student/src/ResourceCentre.java
@@ -30,7 +30,7 @@ public class ResourceCentre {
 				ResourceCentre.setHeader("ITEM TYPES");
 				System.out.println("1. Camcorder");
 				System.out.println("2. Chromebook");
-				//test push and commit 1
+				//test push and commit 2
 				int itemType = Helper.readInt("Enter option to select item type > ");
 
 				if (itemType == 1) {
